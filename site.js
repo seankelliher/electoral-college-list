ba19cee53944d2c7a7ae35ac9b35bd008d415ee7
Sean Kelliher <sbkelliher@icloud.com>
2020-04-03 13:15:39 -0400

    Capitalize hexidecimals in 'electoralCollege' object.

--- site.js
+++ site.js
@@ -19,357 +19,357 @@ voteInfo = {
             state: "Alabama",
             votes: 9,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "alaska",
             state: "Alaska",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "arizona",
             state: "Arizona",
             votes: 11,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "arkansas",
             state: "Arkansas",
             votes: 6,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "california",
             state: "California",
             votes: 55,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "colorado",
             state: "Colorado",
             votes: 9,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "connecticut",
             state: "Connecticut",
             votes: 7,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "delaware",
             state: "Delaware",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "florida",
             state: "Florida",
             votes: 29,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "georgia",
             state: "Georgia",
             votes: 16,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "hawaii",
             state: "Hawaii",
             votes: 4,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "idaho",
             state: "Idaho",
             votes: 4,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "illinois",
             state: "Illinois",
             votes: 20,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "indiana",
             state: "Indiana",
             votes: 11,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "iowa",
             state: "Iowa",
             votes: 6,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "kansas",
             state: "Kansas",
             votes: 6,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "kentucky",
             state: "Kentucky",
             votes: 8,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "louisiana",
             state: "Louisiana",
             votes: 8,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "maine",
             state: "Maine",
             votes: 4,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "maryland",
             state: "Maryland",
             votes: 10,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "massachusetts",
             state: "Massachusetts",
             votes: 11,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "michigan",
             state: "Michigan",
             votes: 16,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "minnesota",
             state: "Minnesota",
             votes: 10,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "mississippi",
             state: "Mississippi",
             votes: 6,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "missouri",
             state: "Missouri",
             votes: 10,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "montana",
             state: "Montana",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "nebraska",
             state: "Nebraska",
             votes: 5,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "nevada",
             state: "Nevada",
             votes: 6,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "newhampshire",
             state: "New Hampshire",
             votes: 4,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "newjersey",
             state: "New Jersey",
             votes: 14,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "newmexico",
             state: "New Mexico",
             votes: 5,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "newyork",
             state: "New York",
             votes: 29,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "northcarolina",
             state: "North Carolina",
             votes: 15,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "northdakota",
             state: "North Dakota",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "ohio",
             state: "Ohio",
             votes: 18,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "oklahoma",
             state: "Oklahoma",
             votes: 7,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "oregon",
             state: "Oregon",
             votes: 7,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "pennsylvania",
             state: "Pennsylvania",
             votes: 20,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "rhodeisland",
             state: "Rhode Island",
             votes: 4,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "southcarolina",
             state: "South Carolina",
             votes: 9,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "southdakota",
             state: "South Dakota",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "tennessee",
             state: "Tennessee",
             votes: 11,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "texas",
             state: "Texas",
             votes: 38,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "utah",
             state: "Utah",
             votes: 6,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "vermont",
             state: "Vermont",
             votes: 3,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "virginia",
             state: "Virginia",
             votes: 13,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "washington",
             state: "Washington",
             votes: 12,
             w2016: "Clinton",
-            c2016: "#03a9f4"
+            c2016: "#03A9F4"
         },
         {
             identity: "washingtondc",
             state: "Washington DC",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "westvirginia",
             state: "West Virginia",
             votes: 5,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "wisconsin",
             state: "Wisconsin",
             votes: 10,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         },
         {
             identity: "wyoming",
             state: "Wyoming",
             votes: 3,
             w2016: "Trump",
-            c2016: "#f44336"
+            c2016: "#F44336"
         }
     ],
 
